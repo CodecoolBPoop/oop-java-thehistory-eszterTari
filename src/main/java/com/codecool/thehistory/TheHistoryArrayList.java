e14ff096e23ec15621a56114be473209501ca50c
eszterTari <tari.eszter.eng@gmail.com>
2018-10-15 11:54:08 +0200

    ArrayList features implemented, except replaceMoreWords

--- src/main/java/com/codecool/thehistory/TheHistoryArrayList.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArrayList.java
@@ -1,9 +1,8 @@
 package com.codecool.thehistory;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
+
 
 public class TheHistoryArrayList implements TheHistory {
     /**
@@ -13,28 +12,33 @@ public class TheHistoryArrayList implements TheHistory {
 
     @Override
     public void add(String text) {
-        //TODO: check the TheHistory interface for more information
+        wordsArrayList = new ArrayList<>(Arrays.asList(text.split("\\s")));
     }
 
     @Override
     public void removeWord(String wordToBeRemoved) {
-        //TODO: check the TheHistory interface for more information
+        for (int i = wordsArrayList.size()-1; i >=0 ; i--) {
+            String word = wordsArrayList.get(i);
+            if (word.equals(wordToBeRemoved)) wordsArrayList.remove(i);
+        }
     }
 
     @Override
     public int size() {
-        //TODO: check the TheHistory interface for more information
-        return 0;
+        return wordsArrayList.size();
     }
 
     @Override
     public void clear() {
-        //TODO: check the TheHistory interface for more information
+        wordsArrayList.clear();
     }
 
     @Override
     public void replaceOneWord(String from, String to) {
-        //TODO: check the TheHistory interface for more information
+        for (int i = 0; i < wordsArrayList.size(); i++) {
+            if (wordsArrayList.get(i).equals(from))
+                wordsArrayList.set(i, to);
+        }
     }
 
     @Override
