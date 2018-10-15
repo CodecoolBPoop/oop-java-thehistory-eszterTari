dd6d32c71c0481e98c9d634e0a90b1f9a910f095
eszterTari <tari.eszter.eng@gmail.com>
2018-10-15 13:27:34 +0200

    LinkedList features implemented, except replaceMoreWords

--- src/main/java/com/codecool/thehistory/TheHistoryLinkedList.java
+++ src/main/java/com/codecool/thehistory/TheHistoryLinkedList.java
@@ -1,9 +1,9 @@
 package com.codecool.thehistory;
 
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Arrays;
 
 public class TheHistoryLinkedList implements TheHistory {
     /**
@@ -13,28 +13,35 @@ public class TheHistoryLinkedList implements TheHistory {
 
     @Override
     public void add(String text) {
-        //TODO: check the TheHistory interface for more information
+        wordsLinkedList = new LinkedList<>(Arrays.asList(text.split("\\s")));
     }
 
     @Override
     public void removeWord(String wordToBeRemoved) {
-        //TODO: check the TheHistory interface for more information
+        ListIterator<String> wordsIterator = wordsLinkedList.listIterator();
+        while (wordsIterator.hasNext()) {
+            String word = wordsIterator.next();
+            if (word.equals(wordToBeRemoved)) wordsIterator.remove();
+        }
     }
 
     @Override
     public int size() {
-        //TODO: check the TheHistory interface for more information
-        return 0;
+        return wordsLinkedList.size();
     }
 
     @Override
     public void clear() {
-        //TODO: check the TheHistory interface for more information
+        wordsLinkedList.clear();
     }
 
     @Override
     public void replaceOneWord(String from, String to) {
-        //TODO: check the TheHistory interface for more information
+        ListIterator<String> wordsIterator = wordsLinkedList.listIterator();
+        while (wordsIterator.hasNext()) {
+            String word = wordsIterator.next();
+            if (word.equals(from)) wordsIterator.set(to);
+        }
     }
 
     @Override
